452dbd3e62b6a60c4b5aca78eeb0b9d2380e4556
dorameme <68580993+dorameme@users.noreply.github.com>
2022-08-21 19:02:12 +0900

    리포지토리
    리포지토리에 데이터베이스 접근, DB저장 관리
    새로운 객체를 만들때 좀더 포괄적인 쪽 예를들면 인터페이스로 만드는것이 좋다..나중에 바꿀수있으니..

---
+++ src/main/java/yunsoo_spring/demo/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package yunsoo_spring.demo.repository;
+
+import yunsoo_spring.demo.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
