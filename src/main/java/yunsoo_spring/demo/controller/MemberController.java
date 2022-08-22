2e6bd55654c1928f33b5ddbb7aec2a9022dbd246
dorameme <68580993+dorameme@users.noreply.github.com>
2022-08-22 16:54:40 +0900

    웹 MVC 개발
    홈화면, 웹기능인 등록 조회 추가

--- src/main/java/yunsoo_spring/demo/controller/MemberController.java
+++ src/main/java/yunsoo_spring/demo/controller/MemberController.java
@@ -2,7 +2,13 @@ package yunsoo_spring.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import yunsoo_spring.demo.domain.Member;
 import yunsoo_spring.demo.service.MemberService;
+
+import java.util.List;
 
 @Controller//컴포넌트 스캔과 자동 의존관계 설정
 public class MemberController {
@@ -13,5 +19,25 @@ public class MemberController {
     // 스프링이 주입해준다.
     public MemberController(MemberService memberService){
         this.memberService=memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member=new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members= memberService.findMembers();
+        model.addAttribute("members",members);
+        return "/members/memberList";
     }
 }
